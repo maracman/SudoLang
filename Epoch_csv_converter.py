b32005878a7d082ee2b80d27c082aac05a5345c8
maracman <82516491+maracman@users.noreply.github.com>
2021-06-29 13:24:48 +0100

    converter low res output
    exports only the pixel array at a flexible resolution.

--- Epoch_csv_converter.py
+++ Epoch_csv_converter.py
@@ -2,19 +2,24 @@ import os
 import sys
 import numpy as np
 import math
-import scipy.misc
-import matplotlib.pyplot as plt
+from datetime import date
+from PIL import Image
+from pathlib import Path
 
 divide_resolution = 10
 
-operating_system = sys.platform
-if operating_system == "darwin":
-    dir_path = os.path.sep.join(sys.argv[0].split(os.path.sep)[:-1])
-else:
-    dir_path = os.getcwd()
+def get_path():
+    operating_system = sys.platform
+    if operating_system == "darwin":
+        dir_path = os.path.sep.join(sys.argv[0].split(os.path.sep)[:-1])
 
-incorrect_location = os.path.join(dir_path,'data/outputs/mouse_coords/incorrect/')
-correct_location = os.path.join(dir_path, 'data/outputs/mouse_coords/correct/')
+    else:
+        dir_path = os.getcwd()
+
+    return_path = os.path.join(dir_path, 'data/outputs/mouse_coords/')
+    return return_path
+
+
 
 def convert_to_rgb(c):
     c = c * 100000
@@ -24,18 +29,17 @@ def convert_to_rgb(c):
     return(r,g,b)
 
 
-
 def convert_to_png(folder_loc):
 
     for root,dirs,files in os.walk(folder_loc):
         for file in files:
-            print(os.path.join(root, file))
+
             if file.endswith(".csv"):
-                print(file)
+                print(os.path.join(folder_loc, file))
                 f=open(os.path.join(folder_loc, file), 'r')
                 file_info = np.loadtxt(f, delimiter=",", skiprows=1)
-                #print(file_info)
-                pix_array = np.zeros((round(600/divide_resolution), round(800/divide_resolution), 3), dtype=np.uint8)
+
+                pix_array = np.zeros((round(600/divide_resolution)+1, round(800/divide_resolution)+1, 3), dtype=np.uint8)
                 number = file_info[0][0]
                 print(number)
 
@@ -44,11 +48,25 @@ def convert_to_png(folder_loc):
                     pix_array[round(int(file_info[i,1])/divide_resolution), round(int(file_info[i,0])/divide_resolution) ] \
                         = [colour_r, colour_g, colour_b]
 
-                plt.imshow(pix_array)
-                plt.show()
+                #create file path
+                day_today = date.today().strftime("%Y-%m-%d")
+                new_path = os.path.join(folder_loc, day_today + "_PNGconvert")
+                Path(new_path).mkdir(parents=True, exist_ok=True)
                 filename = file.strip(".csv")
-                #scipy.misc.toimage(pix_array, cmin=0.0, cmax=...).save(folder_loc + filename + '.png')
+
+                #save pixel array as png
+                im = Image.fromarray(pix_array)
+                im.save(new_path + '/' + filename + '.png')
+
+                #plt.imshow(pix_array)
+                #plt.show()
+
                 f.close()
 
+# run program
+directory = get_path()
+
+incorrect_location = directory + "incorrect"
+correct_location = directory + "correct"
 convert_to_png(correct_location)
 convert_to_png(incorrect_location)
