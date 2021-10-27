e213b24cb9d7c40cd441f3d844178032f051e353
maracman <82516491+maracman@users.noreply.github.com>
2021-10-27 15:44:40 +0100

    Update Epoch_csv_converter.py

--- Epoch_csv_converter.py
+++ Epoch_csv_converter.py
@@ -7,7 +7,8 @@ from PIL import Image
 from pathlib import Path
 import matplotlib.pyplot as plt
 
-divide_resolution = 10
+divide_resolution = 1
+pix_width = 4
 
 def get_path():
     operating_system = sys.platform
@@ -17,7 +18,7 @@ def get_path():
     else:
         dir_path = os.getcwd()
 
-    return_path = os.path.join(dir_path, 'data/outputs/mouse_coords/')
+    return_path = os.path.join(dir_path, 'outputs/mouse_coords/')
     return return_path
 
 
@@ -38,6 +39,34 @@ def countdown_convert(mouse_timing): #creates rgb consistent relative to final c
     return countdown_array
 
 
+def draw_line(mat, x0, y0, x1, y1, inplace=False):
+    if not (0 <= x0 < mat.shape[0] and 0 <= x1 < mat.shape[0] and
+            0 <= y0 < mat.shape[1] and 0 <= y1 < mat.shape[1]):
+        raise ValueError('Invalid coordinates.')
+    if not inplace:
+        mat = mat.copy()
+    if (x0, y0) == (x1, y1):
+        mat[x0, y0] = 2
+        return mat if not inplace else None
+    # Swap axes if Y slope is smaller than X slope
+    transpose = abs(x1 - x0) < abs(y1 - y0)
+    if transpose:
+        mat = mat.T
+        x0, y0, x1, y1 = y0, x0, y1, x1
+    # Swap line direction to go left-to-right if necessary
+    if x0 > x1:
+        x0, y0, x1, y1 = x1, y1, x0, y0
+    # Write line ends
+    mat[x0, y0] = 2
+    mat[x1, y1] = 2
+    # Compute intermediate coordinates using line equation
+    x = np.arange(x0 + 1, x1)
+    y = np.round(((y1 - y0) / (x1 - x0)) * (x - x0) + y0).astype(x.dtype)
+    # Write intermediate coordinates
+    mat[x, y] = 1
+    if not inplace:
+        return mat if not transpose else mat.T
+
 def convert_to_png(folder_loc):
 
     for root,dirs,files in os.walk(folder_loc):
@@ -48,14 +77,39 @@ def convert_to_png(folder_loc):
                 f=open(os.path.join(folder_loc, file), 'r')
                 file_info = np.loadtxt(f, delimiter=",", skiprows=1)
 
-                pix_array = np.zeros((round(600/divide_resolution)+1, round(800/divide_resolution)+1, 3), dtype=np.uint8)
+                pix_array = np.zeros((round(600/divide_resolution)+pix_width+1, round(800/divide_resolution)+pix_width+1, 3), dtype=np.uint8)
 
                 countdown_array = countdown_convert(file_info)
 
-                for i in range(len(file_info)):
+                for i in range(len(file_info)-1):
                     colour_r, colour_g, colour_b = convert_to_rgb(countdown_array[i])
-                    pix_array[round(int(file_info[i,1])/divide_resolution), round(int(file_info[i,0])/divide_resolution) ] \
-                        = [colour_r, colour_g, colour_b]
+                    x = round(int(file_info[i,0])/divide_resolution)
+                    y = round(int(file_info[i,1])/divide_resolution)
+                    next_x = round(int(file_info[i+1,0])/divide_resolution)
+                    next_y = round(int(file_info[i+1,1])/divide_resolution)
+
+                    dx = next_x - x
+                    dy = next_y - y
+
+                    while dx > 0 or dy > 0:
+                        pix_array[int(y), int(x)] = [colour_r, colour_g, colour_b]
+
+                        for pix in range(pix_width):
+                            pix_array[int(y), int(x+pix)] = [colour_r, colour_g, colour_b]
+                            pix_array[int(y), int(x-pix)] = [colour_r, colour_g, colour_b]
+                            pix_array[int(y+pix), int(x)] = [colour_r, colour_g, colour_b]
+                            pix_array[int(y-pix), int(x)] = [colour_r, colour_g, colour_b]
+
+                        steps = dx if (dx > dy) else dy
+                        xinc = dx/steps
+                        yinc = dy/steps
+                        x+=xinc
+                        y+=yinc
+                        dx = next_x - x
+                        dy = next_y - y
+
+                    #else:
+                    #    pix_array[y, x] = [colour_r, colour_g, colour_b]
 
                 #create png folder
                 day_today = date.today().strftime("%Y-%m-%d")
@@ -67,8 +121,8 @@ def convert_to_png(folder_loc):
                 im = Image.fromarray(pix_array)
                 im.save(new_path + '/' + filename + '.png')
 
-                #plt.imshow(pix_array)
-                #plt.show()
+                plt.imshow(pix_array)
+                plt.show()
 
                 f.close()
 
